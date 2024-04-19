6b24a503d58d9568ca5b970aa177bc2f5dcdb7df
rohit <rohit@perpixl.com>
2024-04-19 17:17:13 +0200

    rsm:: fixed test case

--- src/test/java/com/gcp/GcpMasterApplicationTests.java
+++ src/test/java/com/gcp/GcpMasterApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.helloworld;
+package com.gcp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HelloworldApplicationTests {
+public class GcpMasterApplicationTests {
 
 	@Test
 	void contextLoads() {
